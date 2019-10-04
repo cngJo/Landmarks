f5458eeeae7955b88bdd3c7c98c503eb7facabe8
Johannes <johannes@przymusinski.de>
2019-10-04 19:50:42 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Johannes Przymusinski on 04.10.19.
+//  Copyright © 2019 jprzymusinski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
